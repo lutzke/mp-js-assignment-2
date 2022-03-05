8f92c7834ee236fe41fd097cb5128b493e9c6fac
lutzke <lutzke@pm.me>
2022-03-05 14:49:55 -0700

    list books & authors

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 // https://morningpants.github.io/100-best-books/static/
 // https://morningpants.github.io/100-best-books/static/images/things-fall-apart.jpg
 
+function writeHTML(bookList) {
+  booksDiv = document.querySelector('#books');
+  bookList = bookList.join('<br>');
+  booksDiv.innerHTML = bookList;
+};
+
 fetch('https://morningpants.github.io/100-best-books/books.json')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(books => books.map(book => `${book.title} by ${book.author}`))
+  .then(books => writeHTML(books));
